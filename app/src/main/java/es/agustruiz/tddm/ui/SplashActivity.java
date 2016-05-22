453ac8280114d780f5d8440a045ff57c6c0afb29
Agustín Ruiz Linares <agustinruizlinares@gmail.com>
2016-05-22 15:40:14 +0200

    [ADD] Splash activity [ADD] Main activity default template with navigation drawer

---
+++ app/src/main/java/es/agustruiz/tddm/ui/SplashActivity.java
@@ -0,0 +1,20 @@
+package es.agustruiz.tddm.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = SplashActivity.class.getName() + "[A]";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
